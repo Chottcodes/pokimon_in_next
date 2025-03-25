e7cfb5ba9cf25f28583a21e93107d544cac999dd
Chottcodes <cwellshott@gmail.com>
2025-03-25 15:55:30 -0700

    creating input component

---
+++ src/app/components/SearchComponent.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const SearchComponent = () => {
+  return (
+    <form className='w-full h-full'>
+        <input type="text" />
+        
+    </form>
+  )
+}
+
+export default SearchComponent
